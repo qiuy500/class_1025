6d9b308e6d647be8c2dca35793fd54d9c8524ba7
qiuy500 <qiuy500@gmail.com>
2022-10-25 21:52:14 +0800

    Initial Commit

---
+++ class_1025/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  class_1025
+//
+//  Created by 暨大附中２ on 2022/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ class_1025/class_1025App.swift
@@ -0,0 +1,17 @@
+//
+//  class_1025App.swift
+//  class_1025
+//
+//  Created by 暨大附中２ on 2022/10/25.
+//
+
+import SwiftUI
+
+@main
+struct class_1025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
